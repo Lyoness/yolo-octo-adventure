2ba27022c6d0407a5a853518c4e9515ad88d808f
Dan Buch <daniel.buch@gmail.com>
2014-10-17 11:12:49 -0400

    timing is something

--- whatever.go
+++ whatever.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/meatballhat/yolo-octo-adventure/sub"
 )
 
 func main() {
-	fmt.Println("wat " + sub.Word())
+	for i := 3; i > 0; i-- {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println("wat " + sub.Word())
+	}
 }
