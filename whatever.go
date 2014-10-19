eed97097170a54f38af3562ffdd08d3edf2a2892
Dan Buch <daniel.buch@gmail.com>
2014-10-19 11:05:34 -0400

    Some godep testing for travis-ci/travis-build#309

--- whatever.go
+++ whatever.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
+	"github.com/codegangsta/cli"
 	"github.com/meatballhat/yolo-octo-adventure/sub"
 )
 
 func main() {
-	for i := 3; i > 0; i-- {
-		time.Sleep(500 * time.Millisecond)
-		fmt.Println("wat " + sub.Word())
+	app := cli.NewApp()
+	app.Action = func(_ *cli.Context) {
+		for i := 3; i > 0; i-- {
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println("wat " + sub.Word())
+		}
 	}
+
+	app.Run(os.Args)
 }
