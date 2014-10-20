4f46f6fdd0338a655cf9b0f59a304da03f8c7b53
Dan Buch <daniel.buch@gmail.com>
2014-10-19 22:58:29 -0400

    bump

--- whatever.go
+++ whatever.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"time"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/cli"
 	"github.com/meatballhat/yolo-octo-adventure/sub"
 )
 
 func main() {
+	log := logrus.New()
 	app := cli.NewApp()
 	app.Action = func(_ *cli.Context) {
 		for i := 3; i > 0; i-- {
 			time.Sleep(500 * time.Millisecond)
-			fmt.Println("wat " + sub.Word())
+			log.Info("wat " + sub.Word())
 		}
 	}
 
