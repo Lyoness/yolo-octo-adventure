80ae6552917bcc401fde476bf117d23df1bbe055
Dan Buch <daniel.buch@gmail.com>
2015-03-03 13:39:20 -0500

    whatever localhost

--- spec/features/tunnel_spec.rb
+++ spec/features/tunnel_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'localhost root', sauce: true do
   it 'Does not explode' do
-    visit '/'
+    visit 'http://localhost:8000/'
     page.should have_content 'oh hai'
   end
 end
