b5390a17f2ea9d6a88a06227a879dfb6e4a2f531
Dan Buch <daniel.buch@gmail.com>
2016-11-30 15:37:50 -0500

    Connect to mysql better

--- mysql-test.php
+++ mysql-test.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli('localhost');
+$db = new mysqli('localhost', 'root', 'travis', 'travis');
 
 if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');
